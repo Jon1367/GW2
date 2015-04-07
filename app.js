3e5bd783ed3d8a122ce4de34a9dd0bbb9aaba47d
Jonathan Ortiz <jonathano1367@yahoo.com>
2015-04-07 15:57:20 -0700

    form uploads to database

--- app.js
+++ app.js
@@ -5,73 +5,79 @@ var mod = require('./modules/modules.js');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-// Connecting to MYSQL Data Base
+// Connfig to MYSQL Data Base
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  port     :  '8889',
+  database : 'projectdb',
+  user     : 'root',
+  password : 'root',
+});
 
-// var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   port     :  '8889',
-//   database : 'projectdb',
-//   user     : 'root',
-//   password : 'root',
-// });
-
-// connection.connect();
+// connect to MYSQL Database
+connection.connect();
 
 //app use
 app.use(bodyParser.urlencoded({extended: false}));
 
-// Retrieve
+// config Mongo DB
 var mongoose = require('mongoose');
 
 // connect to database
-mongoose.connect('mongodb://localhost/test');
-
-//modal for database
-var Cat = mongoose.model('Cat', { name: String });
-
-// insert into data base 
-var kitty = new Cat({ name: 'Jon' });
-kitty.save(function (err) {
-  if (err) // ...
-  console.log('meow');
-});
+mongoose.connect('mongodb://localhost/test'); // Test name of DB
 
 
 //Routes
 app.get('/', function(req, res) {
 
-var a = 2;
-var b =3;
-
-var add = mod.add(a,b);
-console.log(add);
+// Test modules
+// var a = 2;
+// var b =3;
+// var add = mod.add(a,b);
+// console.log(add);
 
 	res.sendfile(path.join(__dirname + '/views/index.html'));
 
 });
-app.post('/upload',function(req, res){
+
+//Process Forms
+app.post('/uploadMySQL',function(req, res){
+
+	// Grabbing form info
+	var name = req.body.username;
+	var password = req.body.password;
+	console.log(name);
+	console.log(password);
+
+	// Query statement to insert into database
+	connection.query('insert into users(username,password)values(?,?)',[name,password],function(err, rows) {
+		console.log("hello"); 
+		console.log(rows);
+	});
+
+});
+app.post('/uploadMongoDB',function(req, res){
 
 
-  // Grabbing form info
+	// Grabbing form info
+	var username = req.body.username;
+	var password = req.body.password;
+	console.log(username);
+	console.log(password);
 
-// var name = req.body.username;
-// var password = req.body.password;
-// console.log(name);
-// console.log(password);
+	//modal for database
+	var Cat = mongoose.model('Cat', { name: String });
 
-// Query statement to insert into database
-
-// connection.query('insert into users(username,password)values(?,?)',[name,password],function(err, rows) {
-// 	console.log("hello"); 
-// 	console.log(rows);
-// });
-
-
+	// insert into data base 
+	var kitty = new Cat({ name: username });
+	kitty.save(function (err) {
+	  if (err) // ...
+	  console.log('meow');
+	});
 
 
 });
-
 
 app.listen(8080)
 console.log('listening to port 3000'); 
